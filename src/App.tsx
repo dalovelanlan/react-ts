bed8d0388d2fb670316bf52fea41f2b95725e30a
dalovelanlan <15138027270@163.com>
2023-02-26 20:38:22 +0800

    调整头部字体样式

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header text-center">
         <img src={logo} className="App-logo" alt="logo" />
         <p
           onClick={() => {
